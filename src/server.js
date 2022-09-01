fa5acb7549e07c387cbbfb50b34af01da63ab16e
Lucas Carvalho <104842709+lucasdmmc@users.noreply.github.com>
2022-08-31 22:51:56 -0300

    Added dabate base on project

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 require("express-async-errors")
 
-const express = require("express")
+const migrationsRun = require("./database/sqlite/migrations")
 
+const express = require("express")
 const routes = require("./routes")
+migrationsRun()
 
 const AppError =  require("./utils/appError.js")
 
@@ -12,6 +14,7 @@ app.use(express.json())
 
 app.use(routes)
 
+
 app.use((error, request, response, next) => {
   if(error instanceof AppError) {
     return response.status(error.statusCode).json({
@@ -20,6 +23,7 @@ app.use((error, request, response, next) => {
     })
   }
 
+  console.error(error)
   return response.status(500).json({
     message: "error",
     statusCode: "Internal server error"
