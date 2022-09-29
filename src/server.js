62fd72954906b3751bd946eebe3b1c8914cf28ec
lucasdmmc <lucas.dmmc@hotmail.com>
2022-09-29 09:21:34 -0300

    Adicionei env e pm2 no projeto

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+require("dotenv/config")
 require("express-async-errors")
 const migrationsRun = require("./database/sqlite/migrations")
 const AppError =  require("./utils/appError.js")
@@ -34,5 +35,5 @@ app.use((error, request, response, next) => {
 })
 
 
-const PORT = 3333
+const PORT = process.env.SERVER_PORT || 3000
 app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`))
