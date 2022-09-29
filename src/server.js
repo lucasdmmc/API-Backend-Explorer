0245dc4d8c8f62cdb62e416063f99bcf6b83783c
lucasdmmc <lucas.dmmc@hotmail.com>
2022-09-29 09:42:56 -0300

    Ajustando PORT do servidor

--- src/server.js
+++ src/server.js
@@ -35,5 +35,5 @@ app.use((error, request, response, next) => {
 })
 
 
-const PORT = process.env.SERVER_PORT || 3000
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`))
